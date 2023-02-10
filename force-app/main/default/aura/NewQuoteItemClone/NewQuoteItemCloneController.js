f95a110e1b020228a8908e5baf44dd406e130f1d
Jaimin <jaiminshah555@gmail.com>
2023-02-10 16:44:33 +0530

    sab khatam aaj

--- force-app/main/default/aura/NewQuoteItemClone/NewQuoteItemCloneController.js
+++ force-app/main/default/aura/NewQuoteItemClone/NewQuoteItemCloneController.js
@@ -2350,8 +2350,8 @@ return other.Id == current.Id
                                             }
                                         }
                                         } */
-        console.log("Final List :------------------> " + selectedRowList)
-        component.set("v.listOfSelectedIds", selectedRowList)
+        console.log("Final List :------------------> " + NewselectedRows)
+        component.set("v.listOfSelectedIds", NewselectedRows)
         component.set("v.selectedRows", NewselectedRows);
     },
 
@@ -2359,6 +2359,16 @@ return other.Id == current.Id
         var abc = component.get("v.listOfSelectedIds");
         component.set("v.openQuoteLineBox", false);
         component.set("v.openProductBox", true);
+        console.log("cancelBox");
+        // component.set("v.data1", []) //1
+        // component.set("v.selectedRows", "");
+        // component.set("v.data2", "");
+        // component.set("v.data2", []); //2
+        // component.set("v.listOfSelectedIds", []); //3
+        //console data1, data2, listOfSelectedIds
+        console.log("data1 : " + component.get("v.data1"));
+        console.log("data2 : " + component.get("v.data2"));
+        console.log("listOfSelectedIds : " + component.get("v.listOfSelectedIds"));
     },
 
     addQuoteLine: function(component, event, helper) {
@@ -2523,6 +2533,8 @@ return other.Id == current.Id
 
     },
 
+    // *** BOM Grouping ***
+
     onclickBOMGrouping : function(component, event, helper){
         component.set("v.isBOMmodalOpen", true);
         var opts = [
@@ -2571,11 +2583,10 @@ return other.Id == current.Id
             }
             console.log('selectedFieldList ==> ',{selectedFieldList});
             component.set("v.isBOMmodalOpen", false); 
-
-            let recordId = component.get("v.recordId");
             component.set("v.displayGrouping", true);
-            var findcomp = component.find('groupCmp');
-            findcomp.renderCmp(true, recordId, selectedFieldList);
+
+            component.set("v.groupFieldList", selectedFieldList);
+            helper.getQuoteGrouping(component, event, helper);
 
         }
 
@@ -2583,7 +2594,148 @@ return other.Id == current.Id
 
      returnToNormalVIew: function(component, event, helper){
         component.set("v.displayGrouping", false);
-     }
+     }, 
+
+    expandCollapeAll: function(component, event, helper){
+        var QuoteLineWrapper = component.get("v.QuoteLineWrapper");
+
+        var iconName = event.currentTarget.dataset.iconname;
+        var recordId = component.get("v.recordId");
+
+        var expandallIcon = document.getElementById("expandAllBtn_" + recordId);
+        var collapeallIcon = document.getElementById("collapeseAllBtn_" + recordId);
+
+        let group1 = QuoteLineWrapper.groupWrapper;
+
+        if (iconName == 'Expand All') {
+            collapeallIcon.style.display = 'block';
+            expandallIcon.style.display = 'none';
+
+            if (component.get("v.forthGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let group3 = group2[j-1].quoteLineList;
+                        for (let k = 1; k <= group3.length; k++) {
+                            let group4 = group3[k-1].quoteLineList;
+                            for (let l = 1; l <= group4.length; l++) {
+                                let spanGroupId = i+''+j+''+k+''+l;
+                                helper.expandRecordsHelper(component, event, helper, spanGroupId);
+                            }
+                        }
+                    }
+                }
+            } else if (component.get("v.thirdGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let group3 = group2[j-1].quoteLineList;
+                        for (let k = 1; k <= group3.length; k++) {
+                            let spanGroupId = i+''+j+''+k;
+                            helper.expandRecordsHelper(component, event, helper, spanGroupId);
+                        }
+                    }
+                }
+            } else if (component.get("v.secondGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let spanGroupId = i+''+j;
+                        helper.expandRecordsHelper(component, event, helper, spanGroupId);
+                    }
+                }
+            } else if(component.get("v.firstGrouping")){
+                for (let i = 1; i <= group1.length; i++) {
+                    let spanGroupId = i;
+                    helper.expandRecordsHelper(component, event, helper, spanGroupId);
+                }
+            }
+            component.set("v.CollapeCount", 0);
+        } else if (iconName == 'Collapse All') {
+            collapeallIcon.style.display = 'none';
+            expandallIcon.style.display = 'block';
+
+            if (component.get("v.forthGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let group3 = group2[j-1].quoteLineList;
+                        for (let k = 1; k <= group3.length; k++) {
+                            let group4 = group3[k-1].quoteLineList;
+                            for (let l = 1; l <= group4.length; l++) {
+                                let spanGroupId = i+''+j+''+k+''+l;
+                                helper.collapeRecordsHelper(component, event, helper, spanGroupId);
+                            }
+                        }
+                    }
+                }
+            } else if (component.get("v.thirdGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let group3 = group2[j-1].quoteLineList;
+                        for (let k = 1; k <= group3.length; k++) {
+                            let spanGroupId = i+''+j+''+k;
+                            helper.collapeRecordsHelper(component, event, helper, spanGroupId);
+                        }
+                    }
+                }
+            } else if (component.get("v.secondGrouping")) {
+                for (let i = 1; i <= group1.length; i++) {
+                    let group2 = group1[i-1].quoteLineList;
+                    for (let j = 1; j <= group2.length; j++) {
+                        let spanGroupId = i+''+j;
+                        helper.collapeRecordsHelper(component, event, helper, spanGroupId);
+                    }
+                }
+            } else if(component.get("v.firstGrouping")){
+                for (let i = 1; i <= group1.length; i++) {
+                    let spanGroupId = i;
+                    helper.collapeRecordsHelper(component, event, helper, spanGroupId);
+                }
+            }
+            component.set("v.CollapeCount", component.get("v.TotalRecordCount"));
+        }
+    },
+
+    expandCollapeGroup: function(component, event, helper){
+        var recordId = component.get("v.recordId");
+
+        var expandallIcon = document.getElementById("expandAllBtn_" + recordId);
+        var collapeallIcon = document.getElementById("collapeseAllBtn_" + recordId);
+
+        var iconName = event.currentTarget.dataset.iconname;
+        var spanId = event.target.id;
+
+        var totalRecordCount = component.get("v.TotalRecordCount");
+        var collapeCount = component.get("v.CollapeCount");
+    
+        if (iconName == 'Expand Group') {
+            let spanGroupId = spanId.replace('expandGroupBtn_','');
+            helper.expandRecordsHelper(component, event, helper, spanGroupId);
+
+            let recordDivList = document.getElementsByClassName('record_'+spanGroupId);
+            let selectedRecord = recordDivList.length;
+            collapeCount -= selectedRecord;
+        } else if (iconName == 'Collapse Group') {
+            let spanGroupId = spanId.replace('collapeseGroupBtn_','');
+            helper.collapeRecordsHelper(component, event, helper, spanGroupId);
+
+            let recordDivList = document.getElementsByClassName('record_'+spanGroupId);
+            let selectedRecord = recordDivList.length;
+            collapeCount += selectedRecord;
+        }
+        component.set("v.CollapeCount", collapeCount);
+
+        if(collapeCount == totalRecordCount){
+            collapeallIcon.style.display = 'none';
+            expandallIcon.style.display = 'block';
+        } else{
+            collapeallIcon.style.display = 'block';
+            expandallIcon.style.display = 'none';
+        }
+
+    }
 
 
 })
